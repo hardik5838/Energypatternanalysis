028be4336d24476d74b58225b1a03748df684e86
hardik5838 <hderashri@asepeyo.es>
2026-03-26 12:46:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import os
 import io
 from urllib.parse import quote
 import nilm_calculator 
-import Predicción _NILM_Futura
+import Predicción_NILM_Futura
 from scipy.interpolate import PchipInterpolator
 
 
